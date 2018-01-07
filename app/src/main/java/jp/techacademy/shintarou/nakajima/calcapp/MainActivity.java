3e7c5ea8e5f05b5c724ba89fd9db2da1be38f0ce
nakajimashintarou <gitgitgithub@gmail.com>
2018-01-07 11:51:39 +0900

    ボタンを加減乗除の４つ設定したいが最後のボタンしか表示されない

--- app/src/main/java/jp/techacademy/shintarou/nakajima/calcapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/shintarou/nakajima/calcapp/MainActivity.java
@@ -12,14 +12,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /* +ボタン */
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+        /* -ボタン */
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+        /* xボタン */
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+        /* ボタン */
+        Button button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(this);
 
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(this);
+        //変数の設定
+
     }
 
     @Override
+    /*どのボタンが押されたかを判定するif文？　値をSecondActivityへIntent？*/
     public void onClick(View v) {
         Intent intent = new Intent(this, SecondActivity.class);
+        //VALUE1のvalueは、一個目入力EditTextの値
+        intent.putExtra("VALUE1", 10);
+        //VALUE2のvalueは、二個目入力EditTextの値
+        intent.putExtra("VALUE2", 20);
         startActivity(intent);
     }
 }
