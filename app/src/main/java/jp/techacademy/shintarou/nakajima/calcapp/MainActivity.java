d5eb8ab87fb749553e8fb8dbc69fc3af2b94a8fb
nakajimashintarou <gitgitgithub@gmail.com>
2018-01-08 18:23:43 +0900

    加減乗除の４つのボタンレイアウトを設定（できた！）

--- app/src/main/java/jp/techacademy/shintarou/nakajima/calcapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/shintarou/nakajima/calcapp/MainActivity.java
@@ -25,18 +25,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button button4 = (Button) findViewById(R.id.button4);
         button4.setOnClickListener(this);
 
-        //変数の設定
+        //EditTextの変数の設定
 
     }
 
     @Override
-    /*どのボタンが押されたかを判定するif文？　値をSecondActivityへIntent？*/
+    /*
+    ・どのボタンが押されたかを判定するif文？　それともSecondActivity.javaにて判定？
+    ・値をSecondActivityへIntent？
+    */
     public void onClick(View v) {
         Intent intent = new Intent(this, SecondActivity.class);
         //VALUE1のvalueは、一個目入力EditTextの値
         intent.putExtra("VALUE1", 10);
-        //VALUE2のvalueは、二個目入力EditTextの値
+        //VALUE2のvalueは、個目入力EditTextの値
         intent.putExtra("VALUE2", 20);
+        //VALUE3のvalueは、どのボタンを押したか
+        intent.putExtra("VALUE3", );
         startActivity(intent);
     }
 }
