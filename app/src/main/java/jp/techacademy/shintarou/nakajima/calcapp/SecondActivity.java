5ff3a6b80c09449b8d2d8b541d91db91d9d4a8de
nakajimashintarou <gitgitgithub@gmail.com>
2018-01-08 20:45:05 +0900

    "MainActivity 2.class"という謎なクラス。。

--- app/src/main/java/jp/techacademy/shintarou/nakajima/calcapp/SecondActivity.java
+++ app/src/main/java/jp/techacademy/shintarou/nakajima/calcapp/SecondActivity.java
@@ -13,10 +13,27 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
 
         Intent intent = getIntent();
-        int value1 = intent.getIntExtra("VALUE1", 0);
-        int value2 = intent.getIntExtra("VALUE2", 0);
+        String value1 = intent.getStringExtra("VALUE1");
+        String value2 = intent.getStringExtra("VALUE2");
 
+        //どのボタンが押されたかを判定するif文？
+        //足し算
         TextView textView = (TextView) findViewById(R.id.textView);
         textView.setText(String.valueOf(value1 + value2));
+        //引き算
+        /*
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(String.valueOf(value1 - value2));
+        */
+        //掛け算
+        /*
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(String.valueOf(value1 * value2));
+        */
+        //割り算
+        /*
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(String.valueOf(value1 / value2));
+        */
     }
 }
